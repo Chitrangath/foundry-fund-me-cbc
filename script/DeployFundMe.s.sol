3187bf46dd8dd011f8b0f91b203267ea23bfdaa7
Chitrangath <chitrangath2003@gmail.com>
2025-07-14 12:12:48 +0000

    Changes to FundMeTest regarding MockV3Aggregator twice

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -3,22 +3,31 @@ pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
 import {FundMe} from "../src/FundMe.sol";
-import {console} from "forge-std/console.sol";
-import {HelperConfig} from "./HelperConfig.s.sol";
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
 
 contract DeployFundMe is Script {
+    uint8 public constant DECIMALS = 8;
+    int256 public constant INITIAL_ANSWER = 2000e8; // Simulate $2000 ETH/USD
+
+    address public constant SEPOLIA_ETH_USD_FEED = 0x694AA1769357215DE4FAC081bf1f309aDC325306;
+
     function run() external returns (FundMe) {
-        //Before startBroadcast --> Not a "real" txn, done to save gas
-        
-        HelperConfig helperConfig = new HelperConfig();
-        address ethUsdPricefeed = helperConfig.activeNetworkConfig();
-
-
-        //After stopBroadcast --> A "real" txn
         vm.startBroadcast();
-        FundMe fundMe = new FundMe(ethUsdPricefeed);
+
+        address priceFeed;
+        if (block.chainid == 11155111) {
+            // Sepolia testnet
+            priceFeed = SEPOLIA_ETH_USD_FEED;
+        } else {
+            // Local Anvil/Hardhat chain - deploy mock
+            MockV3Aggregator mock = new MockV3Aggregator(DECIMALS, INITIAL_ANSWER);
+            priceFeed = address(mock);
+        }
+
+        FundMe fundMe = new FundMe(priceFeed);
+
         vm.stopBroadcast();
-        console.log("FundMe deployed to:", address(fundMe));
         return fundMe;
     }
 }
+
